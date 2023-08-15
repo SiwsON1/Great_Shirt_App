7f6ec53b5588e2fd52f0e7c117b331056ed4c098
Marcin Siwonia <mahinek12@gmail.com>
2023-08-15 15:23:38 +0200

    Add connect string fixes

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { PrismaService } from './shared/services/prisma.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  if (process.env.DB_connect) {
+    process.env.DATABASE_URL = process.env.DB_connect;
+  } else {
+    process.env.DATABASE_URL =
+      'mysql://root:9aa80452@localhost:3306/greatshirtdb';
+  }
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
